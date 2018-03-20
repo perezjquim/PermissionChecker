5f628b0d7f565cd565362e9e95dd564edc5ad5b8
Joaquim Perez <jquim1@hotmail.com>
2018-03-20 11:34:44 +0000

    Package issues solved (i guess)

--- app/src/main/java/com/perezjquim/permissionchecker/MainActivity.java
+++ app/src/main/java/com/perezjquim/permissionchecker/MainActivity.java
@@ -3,11 +3,14 @@ package com.perezjquim.permissionchecker;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.perezjquim.PermissionChecker;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        PermissionChecker x;
     }
 }
